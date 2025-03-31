cc31d533fec146bf5c5e62e2bd06d6b763a5b104
eveesilvaa <evellynsilvadeveloper@gmail.com>
2025-03-31 16:17:16 -0300

    Feat: Quase finalizando meu portifolio Eba!!!

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -35,7 +35,7 @@ export default function Links({ darkMode }: { darkMode: boolean }) {
     },
     { 
       icon: <FaFilePdf size={24} />, 
-      url: "public/Currículo (1).pdf",
+      url: "src/assets/Currículo (1).pdf",
       label: "Download Resume"
     }
   ];
